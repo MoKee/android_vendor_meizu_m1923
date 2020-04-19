ae7c887adcef2e00ae8abf35bad5ace91d959430
XiNGRZ <chenxingyu92@gmail.com>
2020-04-19 14:14:41 +0800

    m1923: Use charger from stock
    Change-Id: Ia273129cb966194f25ba12f52bc4cb33d9b6ab3c

--- m1923-vendor.mk
+++ m1923-vendor.mk
@@ -19,4 +19,5 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/meizu/m1923/proprietary/lib64/libsdm-disp-apis.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsdm-disp-apis.so \
-    vendor/meizu/m1923/proprietary/lib64/vendor.display.color@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.display.color@1.0.so
+    vendor/meizu/m1923/proprietary/lib64/vendor.display.color@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.display.color@1.0.so \
+    vendor/meizu/m1923/proprietary/bin/charger:$(TARGET_COPY_OUT_SYSTEM)/bin/charger
